a29f513a657b14e031d69b8c1242e43b3e26c27c
i.vanovaleksval@gmail.com <98JHKhuih78BHJByug768^&>
2021-04-13 18:50:07 +0500

    HW1
    - implement methods save, get, delete, clear, getAll, size in Class ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,80 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Resume resume;
+        for (int i = 0; i < storage.length; i++) {
+            resume = storage[i];
+            if (resume != null) {
+                storage[i] = null;
+            } else {
+                break;
+            }
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (Resume resume : storage) {
+            if (resume != null && resume.uuid.equals(uuid)) {
+                return resume;
+            } else {
+                break;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        Resume resume;
+        for (int i = 0; i <= storage.length; i++) {
+            resume = storage[i];
+            if (resume != null && resume.uuid.equals(uuid)) {
+                for (int j = i; j < storage.length; j++) {
+                    if (storage[j] != null) {
+                        storage[j] = storage[j + 1];
+                        i++;
+                    } else {
+                        break;
+                    }
+                }
+            } else {
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int resumeCount = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                resumeCount++;
+            } else {
+                break;
+            }
+        }
+        return Arrays.copyOfRange(storage, 0, resumeCount);
     }
 
     int size() {
-        return 0;
+        int size = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                size++;
+            } else {
+                break;
+            }
+        }
+        return size;
     }
 }
