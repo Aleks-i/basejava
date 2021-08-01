25bde1fa9f97dd1efef29e89842144aa99bef893
i.vanovaleksval@gmail.com <98JHKhuih78BHJByug768^&>
2021-08-01 18:25:21 +0500

    HW 10
    - add Functional interface

--- src/com/urise/webapp/storage/io/serializers/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/io/serializers/DataStreamSerializer.java
@@ -3,52 +3,85 @@ package com.urise.webapp.storage.io.serializers;
 import com.urise.webapp.model.*;
 import com.urise.webapp.model.organization.Link;
 import com.urise.webapp.model.organization.Organization;
+import com.urise.webapp.util.Writer;
 
 import java.io.*;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static com.urise.webapp.util.DateUtil.toStringLocalDate;
 
 public class DataStreamSerializer implements StreamSerializer {
-
-    private void writeStringList(List<String> list, DataOutputStream dos) throws IOException {
-        int sizeListSection = list.size();
-        dos.writeInt(sizeListSection);
-        list.forEach(str -> {
-            try {
-                dos.writeUTF(str);
-            } catch (IOException e) {
-                e.printStackTrace();
+    @Override
+    public void doWrite(Resume resume, OutputStream os) throws IOException {
+        try (DataOutputStream dos = new DataOutputStream(os)) {
+            dos.writeUTF(resume.getUuid());
+            dos.writeUTF(resume.getFullName());
+            Map<ContactType, String> contacts = resume.getContacts();
+            dos.writeInt(contacts.size());
+            for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
+                dos.writeUTF(entry.getKey().name());
+                dos.writeUTF(entry.getValue());
             }
-        });
+
+            for (Map.Entry<SectionType, AbstractSection> entry : resume.getSections().entrySet()) {
+                switch (entry.getKey().name()) {
+                    case "OBJECTIVE", "PERSONAL" -> {
+                        dos.writeUTF(entry.getKey().getTitle());
+                        dos.writeUTF(((TextSection) entry.getValue()).getContent());
+                    }
+                    case "ACHIEVEMENT", "QUALIFICATIONS" -> {
+                        dos.writeUTF(entry.getKey().getTitle());
+                        writeWithException(((ListSection) entry.getValue()).getItems(), dos, dos :: writeUTF);
+                    }
+                    case "EXPERIENCE", "EDUCATION" -> {
+                        dos.writeUTF(entry.getKey().getTitle());
+                        writeWithException(((OrganizationSection) entry.getValue()).getOrganizations(), dos, organization -> {
+                            dos.writeUTF(organization.getHomePage().getName());
+                            dos.writeUTF(organization.getHomePage().getUrl());
+                            writeWithException(organization.getPositions(), dos, position -> {
+                                dos.writeUTF(toStringLocalDate(position.getStartDate()));
+                                dos.writeUTF(toStringLocalDate(position.getEndDate()));
+                                dos.writeUTF(position.getTitle());
+                                String decription = position.getDescription();
+                                dos.writeUTF(decription == null ? "null" : decription);
+                            });
+                        });
+                    }
+                }
+            }
+        }
     }
 
-    private void writeOrganizationList(List<Organization> list, DataOutputStream dos) throws IOException {
-        int sizeOrganizationList = list.size();
-        dos.writeInt(sizeOrganizationList);
-        list.forEach(organization -> {
-            try {
-                dos.writeUTF(organization.getHomePage().getName());
-                dos.writeUTF(organization.getHomePage().getUrl());
-                List<Organization.Position> positionList = organization.getPositions();
-                dos.writeInt(positionList.size());
-                positionList.forEach(position -> {
-                    try {
-                        dos.writeUTF(toStringLocalDate(position.getStartDate()));
-                        dos.writeUTF(toStringLocalDate(position.getEndDate()));
-                        dos.writeUTF(position.getTitle());
-                        String decription = position.getDescription();
-                        dos.writeUTF(decription == null ? "null" : decription);
-                    } catch (IOException ignored) {
-                    }
-                });
-            } catch (IOException ignored) {
+    @Override
+    public Resume doRead(InputStream is) throws IOException {
+        try (DataInputStream dis = new DataInputStream(is)) {
+            Resume resume = new Resume(dis.readUTF(), dis.readUTF());
+            int size = dis.readInt();
+            for (int i = 0; i < size; i++) {
+                resume.addContactData(ContactType.valueOf(dis.readUTF()), dis.readUTF());
             }
-        });
+
+            while (true) {
+                SectionType sectionType = getSectionType(dis);
+                if (sectionType == null) {
+                    break;
+                }
+                switch (sectionType) {
+                    case OBJECTIVE, PERSONAL -> resume.addSection(sectionType, new TextSection(dis.readUTF()));
+                    case ACHIEVEMENT, QUALIFICATIONS -> resume.addSection(sectionType, new ListSection(getStringsForListSection(dis)));
+                    case EXPERIENCE, EDUCATION -> resume.addSection(sectionType, new OrganizationSection(getOrganizationsForOrganizationSection(dis)));
+                }
+            }
+            return resume;
+        }
+    }
+
+    private <T> void writeWithException(Collection<T> collection, DataOutputStream dos, Writer<T> writer) throws IOException {
+        dos.writeInt(collection.size());
+        for (T tCollection : collection) {
+            writer.write(tCollection);
+        }
     }
 
     private SectionType getSectionType(DataInputStream dis) throws IOException {
@@ -88,60 +121,5 @@ public class DataStreamSerializer implements StreamSerializer {
             organizations.add(new Organization(link, positions));
         }
         return organizations;
-    }
-
-    @Override
-    public void doWrite(Resume resume, OutputStream os) throws IOException {
-        try (DataOutputStream dos = new DataOutputStream(os)) {
-            dos.writeUTF(resume.getUuid());
-            dos.writeUTF(resume.getFullName());
-            Map<ContactType, String> contacts = resume.getContacts();
-            dos.writeInt(contacts.size());
-            for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
-                dos.writeUTF(entry.getKey().name());
-                dos.writeUTF(entry.getValue());
-            }
-
-            for (Map.Entry<SectionType, AbstractSection> entry : resume.getSections().entrySet()) {
-                switch (entry.getKey().name()) {
-                    case "OBJECTIVE", "PERSONAL" -> {
-                        dos.writeUTF(entry.getKey().getTitle());
-                        dos.writeUTF(((TextSection) entry.getValue()).getContent());
-                    }
-                    case "ACHIEVEMENT", "QUALIFICATIONS" -> {
-                        dos.writeUTF(entry.getKey().getTitle());
-                        writeStringList(((ListSection) entry.getValue()).getItems(), dos);
-                    }
-                    case "EXPERIENCE", "EDUCATION" -> {
-                        dos.writeUTF(entry.getKey().getTitle());
-                        writeOrganizationList(((OrganizationSection) entry.getValue()).getOrganizations(), dos);
-                    }
-                }
-            }
-        }
-    }
-
-    @Override
-    public Resume doRead(InputStream is) throws IOException {
-        try (DataInputStream dis = new DataInputStream(is)) {
-            Resume resume = new Resume(dis.readUTF(), dis.readUTF());
-            int size = dis.readInt();
-            for (int i = 0; i < size; i++) {
-                resume.addContactData(ContactType.valueOf(dis.readUTF()), dis.readUTF());
-            }
-
-            while (true) {
-                SectionType sectionType = getSectionType(dis);
-                if (sectionType == null) {
-                    break;
-                }
-                switch (sectionType) {
-                    case OBJECTIVE, PERSONAL -> resume.addSection(sectionType, new TextSection(dis.readUTF()));
-                    case ACHIEVEMENT, QUALIFICATIONS -> resume.addSection(sectionType, new ListSection(getStringsForListSection(dis)));
-                    case EXPERIENCE, EDUCATION -> resume.addSection(sectionType, new OrganizationSection(getOrganizationsForOrganizationSection(dis)));
-                }
-            }
-            return resume;
-        }
     }
 }
