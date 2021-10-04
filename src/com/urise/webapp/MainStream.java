646b4b44fcd477cf5465b044adbfab7f5be0ca0e
i.vanovaleksval@gmail.com <98JHKhuih78BHJByug768^&>
2021-10-04 21:24:36 +0500

    HW 12
    - refactor MainStream

--- src/com/urise/webapp/MainStream.java
+++ src/com/urise/webapp/MainStream.java
@@ -7,22 +7,22 @@ import java.util.stream.Collectors;
 public class MainStream {
     public static void main(String[] args) {
         System.out.println(minValue(new int[]{9, 2, 3, 5, 3, 6, 8}));
-        System.out.println(oddOrEven(List.of(1, 5, 8, 6, 4, 1)));
+        System.out.println(oddOrEven(List.of(1, 5, 8, 6, 4)));
     }
 
     private static int minValue(int[] values) {
         return Arrays.stream(values)
                 .distinct()
                 .sorted()
-                .reduce((a, b) -> a * 10 + b)
-                .orElseThrow();
+                .reduce(0, (a, b) -> a * 10 + b);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum = integers.stream()
+                .mapToInt(Integer::intValue).sum();
         return integers
                 .stream()
-                .filter(integers.stream().mapToInt(Integer::intValue)
-                        .sum() % 2 != 0 ? n -> n % 2 == 0 : n -> n % 2 != 0)
+                .filter(sum % 2 != 0 ? n -> n % 2 == 0 : n -> n % 2 != 0)
                 .collect(Collectors.toList());
     }
 }
